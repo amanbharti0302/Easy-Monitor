3faafe362f4c1ecb9d02a990b0d5c50d472815d9
amanbharti0302 <amanbharti0302@gmail.com>
2020-12-30 19:36:05 +0530

    basic setup done

---
+++ src/pages/homepage/homepage.componenet.jsx
@@ -0,0 +1,23 @@
+import react, { Component } from 'react';
+import './homepage.styles.scss';
+
+class Homepage extends Component{
+    constructor(){
+        super();
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <div>hello</div>
+        )
+    }
+
+}
+
+
+
+
+export default Homepage;
