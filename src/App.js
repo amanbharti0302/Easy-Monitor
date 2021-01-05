25d6edad980162a8a64ddc4ab13b37268919d01b
Krishnam <krishnam1998gkp@gmail.com>
2021-01-05 12:33:01 +0530

    Made some changes to master branch to accomodate Dashboard branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
+import React from 'react';
+
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.componenet';
-import Navbar from './components/navbar/navbar.component';
-import Footer from './components/footer/footer.component';
 
-import {Switch,Route,Redirect} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Switch>
-        <Route exact path="/" component={Homepage}></Route>
-      </Switch>
-      <Footer/>
+      <p>Krishnam</p>
     </div>
   );
 }
