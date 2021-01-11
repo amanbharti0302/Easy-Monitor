d474b4eec323037caa2beea5d76532d60ad3cea3
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-01-11 21:18:35 +0530

    adding all subjects functionality

--- src/components/dashboard-main/student/explore-route/explore.jsx
+++ src/components/dashboard-main/student/explore-route/explore.jsx
@@ -1,11 +1,15 @@
 import './explore.css';
 import CardGen from '../../../student-cardgenerator/cardgen.jsx';
 
-const Explore=({subjects})=>{
+const Explore=({currsubject})=>{
+	let cards=[];
+	for(let i=0;i<currsubject.length;i++){
+		cards.push(<CardGen key={i} subject={currsubject[i]}/>)
+	}
 	return(
 		<div>
 			<h2 className="title-text">Explore</h2>
-            <CardGen/>
+            {cards}
 		</div>
 	);
 }
