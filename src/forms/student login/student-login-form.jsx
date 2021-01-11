d390b946eb74c8426a3139a0cb627321759a448a
Krishnam <krishnam1998gkp@gmail.com>
2021-01-11 18:45:15 +0530

    changed styles of form

--- src/forms/student login/student-login-form.jsx
+++ src/forms/student login/student-login-form.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
+import './student-login-form-styles.css';
 
-const StudentLogin = ()=>{
-    return(
-        <div className="form">
-            <h1>Krishnam</h1>
-        </div>
-    )
+class StudentLogin extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            email:'',
+            password:''
+        }
+    }
+
+    render(){
+        return(
+            <div className="form">
+                <div className="form-header">
+                    <h2>LOGIN</h2>
+                </div>
+                <form >
+                    <input type="email" name="email"/>
+                    <input type="password" name="passowrd"/>
+                    <input type="submit" id="submit"/>
+                </form>
+                <div className="form-footer">
+                    <h2>Forgot password?</h2>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 export default StudentLogin;
