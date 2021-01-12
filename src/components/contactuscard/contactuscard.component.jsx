296ce3174621a4f6fa78919b05a0e5e6138989a5
amanbharti0302 <amanbharti0302@gmail.com>
2021-01-12 14:32:40 +0530

    added contact us section to both student and professor

---
+++ src/components/contactuscard/contactuscard.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './contactuscard.styles.css';
+
+import Example from './profilecard.component';
+import apurva from '../../assets/images/apurva.jpeg';
+import aman from '../../assets/images/aman.jpg';
+import sakshi from '../../assets/images/sakshi.jpg';
+import Krishnam from '../../assets/images/propic.jpg';
+
+
+const Contactuscard = ()=>{
+    return(
+        <div className="contactus-card">
+            <h3>Team: Lorem Ipsum 3.0</h3>
+            <h4>College: National Institute Of Technology Patna</h4>
+            <div className="contact-profile">
+            <Example name="Apurva" img={apurva}/>
+            <Example name="Krishnam" img={Krishnam}/>
+            <Example name="Sakshi Sinha" img={sakshi}/>
+            <Example name="Aman Bharti" img={aman}/>
+            </div>
+            <h3></h3>
+        </div>
+    )
+}
+
+
+export default Contactuscard;
--- src/components/dashboard-main/professor/contactus-route/contactus.jsx
+++ src/components/dashboard-main/professor/contactus-route/contactus.jsx
@@ -1,9 +1,11 @@
 import './contactus.css';
+import Contactuscard from '../../../contactuscard/contactuscard.component';
 
 const ContactUs=()=>{
 	return(
 		<div>
 			<h2 className="title-text">Contact Us</h2>
+			<Contactuscard/>
 		</div>
 	);
 }
