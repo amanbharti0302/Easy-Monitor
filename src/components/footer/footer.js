a52ef6acb5a0ee55310fd3ec21a96cc2e88d77d3
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-01-13 09:51:57 +0530

    homepage

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import './footer.css';
+import facebook_icon from '../../assets/images/facebook_icon.png';
+import twitter_icon from '../../assets/images/twitter-icon.png';
+import instagram_icon from '../../assets/images/instagram_icon.png';
+
+const footer=()=>{
+	return(
+		<footer id="footer">
+			<p className="copyright">Copyright &copy; 2020 Easy Company</p>
+			<ul className="social">
+				<li><a href="#"><img src={facebook_icon} className="icon facebook"/></a></li>
+				<li><a href="#"><img src={twitter_icon} className="icon twitter"/></a></li>
+				<li><a href="#"><img src={instagram_icon} className="icon instagram"/></a></li>
+			</ul>
+		</footer>
+	);
+}
+
+export default footer;
---
+++ src/components/homepage-header/header.jsx
@@ -0,0 +1,24 @@
+import './header.css';
+import victory_image from '../../assets/images/slider-icon.png';
+
+const header=({onLogin})=>{
+    return(
+        <div className="header-banner">
+            <header className="header">
+                <div id="company">Easy Monitor</div>
+                <nav className="header-nav">
+                    <div onClick={()=>onLogin('/login-student')} className="login-btn">Student login</div>
+                    <div onClick={()=>onLogin('/login-teacher')} className="login-btn">Professor login</div>
+                </nav>
+            </header>
+            <div id="welcome">
+                <h1>Easy Monitoring is free <strong>for YOU</strong></h1>
+                <p>This template is available for 100% free of charge on TemplateMo. Download, modify and use this for your business website.</p>
+                <div href="#about" className="welcome-btn">Find Out More</div>
+            </div>
+            <img src={victory_image} className="victory-image" alt="First Vector Graphic"/>
+            
+        </div>
+    );
+}
+export default header;
