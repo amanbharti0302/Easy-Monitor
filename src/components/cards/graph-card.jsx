97b3c3d42a59c548b34d31ea5bb62e85c4a73c28
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-01-13 22:50:34 +0530

    graph

--- src/components/cards/graph-card.jsx
+++ src/components/cards/graph-card.jsx
@@ -1,16 +1,40 @@
+import {Bar,Line} from 'react-chartjs-2';
 import './basic-card-styles.css';
 
-const BasicCard = ({title})=>{
+const BasicCard = ({title,data_labels,data_values})=>{
+    const state={
+        labels: data_labels,
+        datasets: [{
+            label: "%Marks",
+            backgroundColor: "rgba(128,10,128,0.8)",
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 1,
+            data: data_values
+        }]
+    }
     return(
-        <div className="card-body">
+        <div className="card-body graph-card">
             <div className="card-head-text">
                 <h2>{title}</h2>
+                <div>
+                    <Bar
+                        data={state}
+                        options={{
+                            legend:{
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+                    />
+                </div>
             </div>
-            <p className="sub-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellat illo ducimus beatae commodi, perspiciatis, repellendus quaerat quae quos atque sapiente id laboriosam totam voluptas soluta! Commodi cum a in.
-            </p>
         </div>
     )
 }
 
 export default BasicCard;
+/*
+<p className="sub-text">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam repellat illo ducimus beatae commodi, perspiciatis, repellendus quaerat quae quos atque sapiente id laboriosam totam voluptas soluta! Commodi cum a in.
+</p>
+*/
--- src/components/explore-subject-cardgen/student/cardgen.jsx
+++ src/components/explore-subject-cardgen/student/cardgen.jsx
@@ -1,12 +1,13 @@
 import '../cardgen.css';
 import BasicCard from '../../cards/basic-card.jsx';
 import GraphCard from '../../cards/graph-card.jsx';
+import RankCard from '../../cards/rank-card.jsx';
 //import LargeCard from '../cards/large-rect-card.jsx';
 
 const cardgen=({currsubject,user})=>{
 	//console.log(currsubject,user);
 	const rankCalc=()=>{
-		
+		return "#1";
 	}
 	if(currsubject==="course")
 		return(
@@ -15,8 +16,8 @@ const cardgen=({currsubject,user})=>{
 	else	
 		return(
 			<div className="cards">
-				<BasicCard title="Class Standing (Ranking in the class)"/>
-				<GraphCard title="Marks Monitor (test wise marks graph)"/>
+				<RankCard rank={rankCalc()}/>
+				<GraphCard title="Marks Monitor (test wise marks graph)" data_labels={['one','two','three','four','five']} data_values={[5,12,13,6,11]}/>
 				<BasicCard title="Attendence Record (shows total classes and attended classes"/>
 			</div>
 		);
