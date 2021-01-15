096d982b49e0daf761e75695942a1219a026a930
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-01-15 19:15:23 +0530

    ..

--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -42,11 +42,13 @@ class App extends Component{
     const {user,currsubject,sideDrawOpen}=this.state;
     return(
       <div className="dashboard">
-          <Header user_subjects={user.subjects} currsubject={currsubject} onSubChange={this.onSubChange} menuToggle={this.menuToggle}/>
-          <div className="dashboard-navbar-main">
-            <NavBar show={sideDrawOpen} isstudent={type==='student'}/>
-            <DashBoardMain user={user} currsubject={currsubject} isstudent={type==='student'}/>
-          </div>
+      <Router>
+        <Header user_subjects={user.subjects} currsubject={currsubject} onSubChange={this.onSubChange} menuToggle={this.menuToggle}/>
+        <div className="dashboard-navbar-main">
+          <NavBar show={sideDrawOpen} isstudent={type==='student'}/>
+          <DashBoardMain user={user} currsubject={currsubject} isstudent={type==='student'}/>
+        </div>
+      </Router>
       </div>
     )
   }
