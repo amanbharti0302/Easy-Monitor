6e8d71de349c8e3920eedd5a51d07edde8867e54
amanbharti0302 <amanbharti0302@gmail.com>
2021-01-16 02:45:08 +0530

    file upload completed

--- src/components/uploaded-document/uploaded-document.jsx
+++ src/components/uploaded-document/uploaded-document.jsx
@@ -3,7 +3,7 @@ import './uploaded-styles.css';
 const Uploadedtab = ({data})=>{
   return(
 		<div className="uploaded-myfiles">
-      <h3>{data.Name}</h3>
+      <h3>{data.name}</h3>
       <p className="upload-p">{data.description}</p>
 		</div>
     )
