ab51acb730d53fc08b9c1b43b9beb5751a91c41b
ApurvaP04 <59483865+Apurva-04@users.noreply.github.com>
2021-01-15 20:25:25 +0530

    explore

--- src/components/teacherDashboard/teacherexplore/teacherdashboard-explore.jsx
+++ src/components/teacherDashboard/teacherexplore/teacherdashboard-explore.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import './teacher-dashboard-explore-styles.css';
+import BasicCard from '../../cards/basic-card.jsx';
+import SubjectCard from '../../cards/subject-card.jsx';
+import GraphCard from '../../cards/graph-card.jsx';
+import StudentCard from '../../cards/student-card.jsx';
+import DendoCard from '../../cards/dendogram-card.jsx';
 
 const DashBoardExplore = ()=>{
     return(
@@ -7,9 +12,15 @@ const DashBoardExplore = ()=>{
             <div className="teacher-title">
                 <h2 className="teacher-title-text">Explore</h2>
             </div>
-            
+            <div className="cards">
+				<GraphCard title="Class Performance (test wise)"  data_labels={['one','two','three','four','five']} data_values={[80,82,79,86,91]} y_label="%Avg. Marks"/>
+				<DendoCard/>
+				<StudentCard/>
+			</div>
         </div>
     )
 }
 
 export default DashBoardExplore;
+
+//import LargeCard from '../cards/large-rect-card.jsx';
