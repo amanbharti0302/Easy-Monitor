8d5c1981fd5dddb6951eeea1f58e4df3ead2cdb0
Krishnam <krishnam1998gkp@gmail.com>
2021-01-18 01:59:40 +0530

    Reviewing the code

--- src/components/cards/contactuscards/contact-us-card.jsx
+++ src/components/cards/contactuscards/contact-us-card.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import Github from '../../../assets/images/github.png';
 import LinkedIn from '../../../assets/images/linkedin.png';
-import Krishnam from '../../../assets/images/propic.jpg';
+import {Link} from 'react-router-dom';
 import IconCard from '../icon-card/icon-card-component';
 
 
 import './contact-us-card-style.css';
-const ContactUsCard=()=>{
+const ContactUsCard=({image,name,desig,linkedin,github})=>{
     return(
         <div className="contact-us-wrapper">
             <div className="image-wrapper">
                 <div className="image-outer">
                     <div className="image-inner">
-                        <img src={Krishnam} alt=""/>
+                        <img src={image} alt=""/>
                     </div>
                 </div>
             </div>
             <div className="name">
-                <h2 className="name-text">Krishnam Srivastava</h2>
-                <h2 className="designation">Front End Developer</h2>
+                <h2 className="name-text">{name}</h2>
+                <h2 className="designation">{desig}</h2>
             </div>
             <div className="social-icons">
-                <IconCard image={Github} />
-                <IconCard image={LinkedIn} />
+                <a href={github}><IconCard image={Github} /></a>
+                <a href={linkedin}><IconCard image={LinkedIn} /></a>
             </div>
             <div className="email">
                 <button className="email-btn">Email</button>
