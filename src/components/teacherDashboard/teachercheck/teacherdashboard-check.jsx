25f26983fa0c05c983eee99794ab80a8bc5c44c6
Krishnam <krishnam1998gkp@gmail.com>
2021-01-17 16:07:08 +0530

    Changed the UI of check work

--- src/components/teacherDashboard/teachercheck/teacherdashboard-check.jsx
+++ src/components/teacherDashboard/teachercheck/teacherdashboard-check.jsx
@@ -1,5 +1,4 @@
 import React,{Component} from 'react';
-import CheckResultCard from '../checkResultCard/check-result-card-component';
 import './teacher-dashboard-check-styles.css';
 
 class DashBoardCheck extends Component{
@@ -9,20 +8,10 @@ class DashBoardCheck extends Component{
             show:false
         }
     }
-    handleClick = ()=>{
-        this.setState((prevState)=>{
-            return(
-                {show:!prevState.show}
-            )
-        })
-    }
+    
+    
     render(){
-        let {show} = this.state;
-        let className='show',text='hide';
-        if(show===false){
-            text='Show';
-            className = 'hide';
-        }
+        
         return(
             <div className="teacher-check-body">
                 <div className="teacher-check-title">
@@ -30,20 +19,63 @@ class DashBoardCheck extends Component{
                         Check
                     </h2>
                 </div>
-                <div className="check-work-block">
-                    <div className="check-work-card">
-                        <p className="check-work-text">Check the assigned work for copying?</p>
-                        <button className="check-btn" onClick={this.handleClick}>{text}</button>
-                    </div>
-                    <div className={`check-work-result-block ${className}`}>
-                        <h2 className="check-work-result-head">Results</h2>
-                        <CheckResultCard />
-                        <CheckResultCard />
-                        <CheckResultCard />
-                        <CheckResultCard />
-                    </div>
-                    
+                <div className="table">
+                    <table>
+                        <tr>
+                            <th className="first-head">Sno.</th>
+                            <th className="second-head">Course Code</th>
+                            <th classaName="third-head">Assignement</th>
+                            <th className="fourth-head">No. of Submissions</th>
+                            <th className="fifth-head">Check</th>
+                        </tr>
+                        <tr>
+                            <td className="first">1</td>
+                            <td className="second">EC6501</td>
+                            <td className="third">
+                                <select name="" id="">
+                                    <option value="">Select</option>
+                                    <option value="As 01">As 01</option>
+                                    <option value="As 02">As 02</option>
+                                </select>
+                            </td>
+                            <td className="fourth">3</td>
+                            <td className="fifth">
+                                <button className="check" onClick={this.handleClick}>Check</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="first">2</td>
+                            <td className="second">EC6502</td>
+                            <td classNamee="third">
+                                <select name="" id="">
+                                    <option value="default">Select</option>
+                                    <option value="As 01">As 01</option>
+                                    <option value="As 02">As 02</option>
+                                </select>
+                            </td>
+                            <td className="fourth">4</td>
+                            <td className="fifth">
+                                <button className="check" onClick={this.handleClick}>Check</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="first">3</td>
+                            <td className="second">EC6523</td>
+                            <td className="third">
+                                <select name="" id="">
+                                    <option value="default">Select</option>
+                                    <option value="As 01">As 01</option>
+                                    <option value="As 02">As 02</option>
+                                </select>
+                            </td>
+                            <td className="fourth">6</td>
+                            <td className="fifth">
+                                <button className="check" onClick={this.handleClick}>Check</button>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
+                
             </div>
         )
     }
--- src/components/teacherDashboard/teachercontactus/teacherdashboard-contact.jsx
+++ src/components/teacherDashboard/teachercontactus/teacherdashboard-contact.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import ContactUsCard from '../../cards/contactuscards/contact-us-card';
 import './teacher-dashboard-contact-styles.css';
 
 const DashBoardContact = ()=>{
     return(
         <div className="teacher-contact-body">
-            <div className="teacher-title">
-                <h2 className="teacher-title-text">Contact Us</h2>
+            <div className="teacher-contact-title">
+                <h2 className="teacher-contact-title-text">Contact The Developers</h2>
             </div>
-            
+            <div className="contact-us-cards">
+                <div className="row-1">
+                    <ContactUsCard />
+                    <ContactUsCard />
+                </div>
+                <div className="row-2">
+                    <ContactUsCard />
+                    <ContactUsCard />
+                </div>
+                
+            </div>
         </div>
     )
 }
